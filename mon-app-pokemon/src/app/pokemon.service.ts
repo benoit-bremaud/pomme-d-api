86f6c775bcd71455493a25a2ee8875c6e833bc49
Benoit Bremaud <benoit.bremaud@laplateforme.io>
2024-03-13 12:08:32 +0100

    Add Pokemon service and component

---
+++ mon-app-pokemon/src/app/pokemon.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  constructor(private http: HttpClient) { }
+
+  getPokemon(name: string) {
+    const apiUrl = `https://pokeapi.co/api/v2/pokemon/${name}`;
+    return this.http.get(apiUrl);
+  }
+}
